6e133380ce0e8f239b929ebc23ba8edb9c4d86a6
Simon Risk <simonnrisk@gmail.com>
2025-12-08 14:03:58 -0500

    chore: add titles

--- app/data/videos.ts
+++ app/data/videos.ts
@@ -1,39 +1,33 @@
 export interface Video {
   id: string
   title: string
-  metaTitle: string
   description: string
 }
 
 export const videos: Video[] = [
   {
     id: '2lT_0OFcB9c',
-    title: 'Artwork I',
-    metaTitle: 'Bird shadow',
+    title: 'Bird Adventure Shadow Play',
     description: `We used our bodies and recyled materials to present a short narrative about a bird finding food for its young. We used gels over our light source to convey sunrise, water, and sunset. The medium of shadow afforded transformation; we transformed the worm into mush by hiding it behind the mother bird's head. I added music in post to make it more engaging.`
   },
   {
     id: 'fpRkhBElDCE',
-    title: 'Artwork II',
-    metaTitle: 'Spongebob',
+    title: 'Spongebob Overhead Projection',
     description: `We drew on transparency sheets with markers to make our characters and then cut them out. We printed our backgrounds on transparency sheets. We held a clear glass container filled with water, blue food colouring and glitter under the projector to create the water texture.`
   },
   {
     id: '6jeKLeA-aow',
-    title: 'Artwork III',
-    metaTitle: 'analog',
+    title: 'Analogue Effects Dream Sequence',
     description: `We were inspired by Maya Deren to create this surrealist dream sequence. We used all the analog effects we learned in class in our assignment including shadow work, pepper's ghost, camera obscura, and overhead projection.`
   },
   {
     id: 'hEsxlPjCtAQ',
-    title: 'Artwork IV',
-    metaTitle: 'benadryl',
+    title: 'Data Moshed Benadryl Trip',
     description: `We thought data moshing was a perfect tool for portraying a confusing dream/drug trip. The staticky effect we thought would lend itself well to a headachy, confusing, slightly demonic benadryl trip. The face of our benadryl demon was taken from a 2008 art project called "This Man" or "Dream Man" by sociologist Andrea Natella who claimed a well known New York psychologist ran a study and found that this man had been appearing in thousands of people's dreams. The picture blew up on the internet where many more people claimed they too had seen him, before Natella revealed it was all a hoax and social experiement.`
   },
   {
     id: 'a7pU6FKxjY4',
-    title: 'Artwork V',
-    metaTitle: 'paperworld',
+    title: 'Digital Effects Paperworld',
     description: `We used data moshing, virtual world building, and green screen along with some analog effects like stop motion and drawn backgrounds for this project.`
   }
 ]
